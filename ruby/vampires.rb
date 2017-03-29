97ba49684fcc60da0502e9d33c99d8947a4ad02b
Alex Katzen <malex640@gmail.com>
2017-03-29 13:50:52 -0700

    Add employee questions to employee survey (Release 1)

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+puts "Welcome to Werewolf Inc."
+
+puts "What is your name?"
+employee_name = gets.chomp.downcase
+
+puts "How old are you? What year were you born?"
+employee_bday = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+employee_wants_garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+employee_wants_health_insurance = gets.chomp

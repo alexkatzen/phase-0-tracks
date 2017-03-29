8890170c6db854c7664284904e6d1222dcb52287
Alex Katzen <malex640@gmail.com>
2017-03-29 14:31:44 -0700

    Add logic to determine if employee gave correct age

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -15,3 +15,26 @@ employee_wants_garlic = gets.chomp
 puts "Would you like to enroll in the company’s health insurance?"
 employee_wants_health_insurance = gets.chomp
 
+vampire_evaluation = nil
+wrong_age = false
+
+# Did the employee give the correct age (Does birth year and the given age line up?)
+# If so: “Probably not a vampire.”
+if employee_birthyear == 2017 - employee_age
+  vampire_evaluation = "Probably not a vampire."
+else 
+  wrong_age = true
+end
+
+# Did the employee give the wrong age, turn down garlic bread, _OR_ waive health insurance?
+# If so: “Probably a vampire.”
+
+
+# Did the employee give the wrong age, turn down garlic bread, _AND_ waive health insurance?
+# If so: "Almost certainly a vampire.”
+
+# Is the employee's name "Drake Cula" _OR_ “Tu Fang”?
+# If so: "Definitely a vampire"
+
+# Otherwise:
+# "Results inconclusive"

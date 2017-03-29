98a735fa8632741d773aacdd3b769df478eb90e9
Alex Katzen <malex640@gmail.com>
2017-03-29 14:06:05 -0700

    Break up employee age and employee birthyear into two questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,11 +3,15 @@ puts "Welcome to Werewolf Inc."
 puts "What is your name?"
 employee_name = gets.chomp.downcase
 
-puts "How old are you? What year were you born?"
-employee_bday = gets.chomp
+puts "How old are you?" 
+employee_age = gets.chomp.to_i
+
+puts "What year were you born?"
+employee_birthyear = gets.chomp.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 employee_wants_garlic = gets.chomp
 
 puts "Would you like to enroll in the company’s health insurance?"
 employee_wants_health_insurance = gets.chomp
+

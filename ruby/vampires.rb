f3a3dd24cf15a4bd9a2d0f753a36c8bcc9ac1d97
Alex Katzen <malex640@gmail.com>
2017-03-29 14:42:41 -0700

    Add logic to determine wrong age, turn down garlic bread, _OR_ waive health insurance

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -9,13 +9,13 @@ employee_age = gets.chomp.to_i
 puts "What year were you born?"
 employee_birthyear = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-employee_wants_garlic = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n?)"
+employee_wants_garlic = gets.chomp.downcase
 
-puts "Would you like to enroll in the company’s health insurance?"
-employee_wants_health_insurance = gets.chomp
+puts "Would you like to enroll in the company’s health insurance? (y/n?)"
+employee_wants_health_insurance = gets.chomp.downcase
 
-vampire_evaluation = nil
+vampire_evaluation = "Results inconclusive."
 wrong_age = false
 
 # Did the employee give the correct age (Does birth year and the given age line up?)
@@ -28,7 +28,9 @@ end
 
 # Did the employee give the wrong age, turn down garlic bread, _OR_ waive health insurance?
 # If so: “Probably a vampire.”
-
+if (wrong_age && employee_wants_garlic == "n") || employee_wants_health_insurance == "n"
+  vampire_evaluation = "Probably a vampire."
+end
 
 # Did the employee give the wrong age, turn down garlic bread, _AND_ waive health insurance?
 # If so: "Almost certainly a vampire.”
@@ -38,3 +40,5 @@ end
 
 # Otherwise:
 # "Results inconclusive"
+
+puts vampire_evaluation

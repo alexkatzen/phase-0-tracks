dbeff9479d8a28ff69e09193316302c5c7cc469b
Alex Katzen <malex640@gmail.com>
2017-03-30 19:13:41 -0700

    Add decrypt method and tests

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -16,4 +16,44 @@ def encrypt(string)
   return encrypted
 end
 
-puts encrypt("abdcdefghijklmnopqrstuvwxy  z")
+puts encrypt("abcdefghijklmnopqrstuvwxyz  z")
+
+
+def decrypt(string)
+  decrypted = ""
+  counter = 0
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  
+  while counter < string.length
+    
+    letter = string[counter]
+    
+    # if the letter is not an 'a' or ' '
+    if letter != "a" && letter != " "
+
+      # find the index position of the new, encrypted letter.
+      new_letter_index = alphabet.index(letter)-1
+      
+      # add the encrypted letter using the new index position
+      decrypted += alphabet[new_letter_index]
+      counter += 1
+    
+    elsif string[counter] == "a"
+      decrypted += "z"
+      counter += 1
+    elsif string[counter] == " "
+      decrypted += " "
+      counter += 1
+    end
+  end
+  return decrypted
+end
+
+puts encrypt("abc")
+puts encrypt("zed")
+puts decrypt("bcd")
+puts decrypt("afe")
+puts decrypt("d e v b o o t c a m p")
+
+# The below code works because it is decrypting the result of the encrypting method.
+puts decrypt(encrypt("swordfish"))

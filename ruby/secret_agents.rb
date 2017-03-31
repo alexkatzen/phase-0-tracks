5352810f1a64ecbb587488ed4fa47b50cbe2329e
Alex Katzen <malex640@gmail.com>
2017-03-30 19:19:41 -0700

    Simplify code, add better comments

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -26,34 +26,38 @@ def decrypt(string)
   
   while counter < string.length
     
+    # keep track of the letter to be encrypted
     letter = string[counter]
     
     # if the letter is not an 'a' or ' '
     if letter != "a" && letter != " "
 
-      # find the index position of the new, encrypted letter.
+      # find the index position of the new, encrypted letter within the alphabet.
       new_letter_index = alphabet.index(letter)-1
       
       # add the encrypted letter using the new index position
       decrypted += alphabet[new_letter_index]
       counter += 1
     
-    elsif string[counter] == "a"
+    # encrypt with a 'z' if the letter to be encrypted is an 'a'
+    elsif letter == "a"
       decrypted += "z"
       counter += 1
-    elsif string[counter] == " "
-      decrypted += " "
+    
+    # leave spaces alone
+    elsif letter == " "
+      decrypted += letter
       counter += 1
     end
   end
   return decrypted
 end
 
-puts encrypt("abc")
-puts encrypt("zed")
-puts decrypt("bcd")
-puts decrypt("afe")
-puts decrypt("d e v b o o t c a m p")
+# puts encrypt("abc")
+# puts encrypt("zed")
+# puts decrypt("bcd")
+# puts decrypt("afe")
+# puts decrypt("d e v b o o t c a m p")
 
 # The below code works because it is decrypting the result of the encrypting method.
-puts decrypt(encrypt("swordfish"))
+# puts decrypt(encrypt("swordfish"))

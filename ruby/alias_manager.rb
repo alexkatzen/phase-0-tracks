40679199315ab2aa534ccad7c991462dbf4ebaf8
Alex Katzen <malex640@gmail.com>
2017-04-05 16:24:29 -0700

    Add arrays for both characters in first name, last name, and  move vowels and consonants into the translate method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,18 +2,27 @@
 def create_alias(fullname)
 
   # Create an array 'given_name' of the two words using Name.split(' ')
-  given_name = fullname.split(' ')
+  full_name = fullname.split(' ')
 
-  # Store an array of vowels in including both the upper and lower case variations
-  vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-  consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
+  # Array for the characters in the first name
+  first_name = full_name[0].chars
 
-  # Store an array of consonants including both the upper and lower case variations
+  # Array for the characters in the last name
+  last_name = full_name[1].chars
+
 
   # Create a function 'translate' that maps each letter in a string to 
   # the appropriate vowel or consonant:
+  def translate(word)  
+    
+    # Store an array of vowels in including both the upper and lower case variations
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  
+    # Store an array of consonants including both the upper and lower case variations
+    consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
     
     # If the letter is a vowel, map! that that letter to the next vowel
+      
       # If the letter is a 'u' map! the letter to an 'a'
       # Elsif the letter is a 'U' map! the letter to an 'A'
       # Otherwise, get the index position of the of the letter in the vowels array, and map! it to the letter at the next index position.
@@ -24,9 +33,12 @@ def create_alias(fullname)
       # Otherwise, get the index position of the letter in the consonants array, and map! it to the letter at the next index position.
 
     # Else, if the character is not a character in either of the arrays, e.g. a number, assign a random vowel
+  
+  end # end translate
 
-  # Translate the last_name argument and set it to 'encrypted_first_name'
-  # Translate the first_name argument and set it to 'encrypted_last_name'
+  # Translate given_name[0] and set it to 'encrypted_last_name'
+  # Translate given_name[1] and set it to 'encrypted_first_name'
+  
 
   # return 'encrypted_first_name' + 'encrypted_last_name'
 

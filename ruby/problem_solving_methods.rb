b459b038824d4a32590c8a9093bb6a9e4aa6cf72
Alex Katzen <malex640@gmail.com>
2017-04-06 10:42:33 -0700

    Create a hash to hold the items in the array and their corresponding index position values

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -6,20 +6,18 @@ my_array = [21, 45, 25, 6, 2]
 # Define a method that takes an array and an integer
 def search (array, number)
 
-  # Create an empty array that will hold our generated index positions
-  index_positions = []
+  # Create an empty hash that will map the 
+  # items in the given array to their index positions
+  index_positions = {}
 
-  # Loop through the given array. and push an index position for each item 
-  # in the array, starting at 0
-
+  # Loop through the given array, add a key to the hash for each item in the array
+  # with a value of it's index position
   counter = 0
 
   for i in array 
-    index_positions.push(counter)
+    index_positions[i] = counter
     counter += 1
   end
-
-  return index_positions
 
 end
 

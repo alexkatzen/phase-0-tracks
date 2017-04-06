d699d8d50ea60fd46b333544c2df463183a782fe
Alex Katzen <malex640@gmail.com>
2017-04-05 17:29:26 -0700

    Add translating logic

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,43 +8,61 @@ def create_alias(fullname)
   first_name = full_name[0]
   last_name = full_name[1]
 
-
   # Create a function 'translate' that maps each letter in an array to 
   # the appropriate vowel or consonant:
   def translate(word)  
     
     # break the word into an array of letters
-    untranslated = word.chars
+    translated = word.chars
 
-    # Store an array of vowels in including both the upper and lower case variations
-    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-  
-    # Store an array of consonants including both the upper and lower case variations
-    consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
+    # Store an ordered array of vowels and consonants including both upper and lower case characters
+    vowels_and_consonants = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
     
-    # If the letter is a vowel, map! that that letter to the next vowel
+    # map each character in the array to the appropriate next character.
+    translated.map! do |letter|
+
+      # find the index position of the letter in the vowels and consonants array
+      untranslated_index = vowels_and_consonants.index(letter)
       
-      # If the letter is a 'u' map! the letter to an 'a'
+      # If the letter isn't in the vowels and consonants array, set the character to a random letter.
+      if untranslated_index == nil
+        letter = vowels_and_consonants[rand(52)]
+      
+      # Elsif  the letter is a 'u' map! the letter to an 'a'
+      elsif letter == 'u'
+        letter = 'a'
+
       # Elsif the letter is a 'U' map! the letter to an 'A'
-      # Otherwise, get the index position of the of the letter in the vowels array, and map! it to the letter at the next index position.
+      elsif letter == 'U'
+        letter = 'A'
 
-    # Elsif the letter is a consonant, map! that character to the next consonant
-      # If the letter is a 'z', map! the letter to a 'b'
+      # Elsif  the letter is a 'z', map! the letter to a 'b'
+      elsif letter == 'z'
+        letter = 'b'
+
       # Elsif the letter is a 'Z', map! the letter to a 'B'
-      # Otherwise, get the index position of the letter in the consonants array, and map! it to the letter at the next index position.
+      elsif letter == 'Z'
+        letter = 'B'
 
-    # Else, if the character is not a character in either of the arrays, e.g. a number, assign a random vowel
+      # Else, set the letter to the letter at the next index position.
+      else
+       letter = vowels_and_consonants[untranslated_index+1] 
+      end
 
-    return untranslated.join('')
+    end # end translating block
+
+    return translated.join('')
   
   end # end translate
 
   # Translate given_name[0] and set it to 'encrypted_last_name'
   # Translate given_name[1] and set it to 'encrypted_first_name'
+  translate(first_name)
   
 
   # return 'encrypted_first_name' + 'encrypted_last_name'
 
 end # end create_alias
 
-p create_alias('Felicia Torres') # Should become 'Vussit Gimodoe'
+puts create_alias('Felicia Torres') # Should become 'Vussit Gimodoe'
+puts create_alias('123456')

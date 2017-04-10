31051e82ac02b3c61cec509555bdcace217b0d4b
Alex Katzen <malex640@gmail.com>
2017-04-09 17:18:39 -0700

    Add characteristics and behavior to puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Mini-Dane
+
+# CHARACTERISTICS ----------------
+# fur-color: brown
+# bark: loud
+# temperament: energetic
+# name: varies
+# height: 2.5 ft.
+# strength: quite strong
+
+# BEHAVIOR -----------------------
+# taunt
+# smash

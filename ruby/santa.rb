6283bd4a979d802184626c76a4f4681532a87bb0
Alex Katzen <malex640@gmail.com>
2017-04-10 22:27:50 -0700

    Add exercises for release 2 - do the thing

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,12 +17,41 @@ class Santa
     puts "That was a good #{cookie_type} cookie!"
   end
 
+  # get the gender
   def gender
     @gender
   end
 
+  # get the ethnicity
   def ethnicity
     @ethnicity
+  end
+
+  #get age
+  def age
+    @age
+  end
+
+  # get the current reindeer ranking
+  def reindeer_ranking
+    @reindeer_ranking
+  end
+
+  # age the santa by one year
+  def celebrate_birthday
+    @age+= 1
+  end
+
+  # get mad at a reindeer
+  def get_mad_at=(reindeer_name)
+    i = @reindeer_ranking.index(reindeer_name)
+    @reindeer_ranking.push(reindeer_ranking[i])
+    @reindeer_ranking.delete_at(i)
+  end
+
+  # set the gender
+  def set_gender=(gender)
+    @gender = gender
   end
 
 end
@@ -45,3 +74,24 @@ end
 starwars_santas.each do |santa|
   puts "A #{santa.gender} #{santa.ethnicity} was born."
 end
+
+# age mortimer by 3 years
+p mortimer.age
+mortimer.celebrate_birthday
+mortimer.celebrate_birthday
+mortimer.celebrate_birthday
+p mortimer.age
+
+# get mad at Vixen
+p mortimer.reindeer_ranking
+mortimer.get_mad_at=("Vixen")
+p mortimer.reindeer_ranking
+
+# set the gender
+p mortimer.gender
+mortimer.set_gender=("Androgynous")
+p mortimer.gender
+
+# get age and ethnicity
+p mortimer.age
+p mortimer.ethnicity

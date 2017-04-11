0fa1d375d14fd0d6fded3eda5a124b41eda61acb
Alex Katzen <malex640@gmail.com>
2017-04-10 21:01:16 -0700

    Add diverse santa initializations (do the thing - Release 1)

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,7 +7,6 @@ class Santa
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
-    
   end
 
   def speak
@@ -18,8 +17,31 @@ class Santa
     puts "That was a good #{cookie_type} cookie!"
   end
 
+  def gender
+    @gender
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
 end
 
 # DRIVER CODE
-alex = Santa.new
-alex.eat_milk_and_cookies("Oatmeal")
+mortimer = Santa.new("male", "n/a")
+mortimer.eat_milk_and_cookies("Oatmeal")
+
+genders = ["Female", "Male", "Androgynous", "Non-binary", "Spirit", "Other"]
+ethnicities = ["Jawa", "Wookiee", "Rodian", "Hutt", "Gamorrean", "Twi’lek", "Yuzzum", "Ewok", "Sullustan", "Geonosian"]
+
+starwars_santas = []
+
+# Populate starwars_santas with up to 30 santas
+rand(30).times do |santa|
+  starwars_santas << Santa.new(genders[rand(genders.length)], ethnicities[rand(ethnicities.length)])
+end
+
+# Print out starwars_santas
+starwars_santas.each do |santa|
+  puts "A #{santa.gender} #{santa.ethnicity} was born."
+end

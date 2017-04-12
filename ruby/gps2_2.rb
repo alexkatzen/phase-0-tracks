b6a521b3474c03d35be85b028b145a041dfa7e2f
Alex Katzen <malex640@gmail.com>
2017-04-11 17:29:46 -0700

    Add code created during time as Navigator

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,48 +1,58 @@
+# Method to print a list and make it look pretty
+
+# input: the hash
+# steps:
+  # iterate over the hash
+    # print the key and it's value
+# output:
+  #series of strings with key and its appropriate value
+def print_list(grocery_list)
+  grocery_list.each do |item_name, quantity|
+    puts "You have #{quantity} #{item_name}"
+  end
+  puts "You are done shopping!"
+  puts "-------" #for readability
+end
+
+# - - - -
 # Method to create a list
-# input: string of items separated by spaces (example: "carrots apples cereal pizza")
 
-# steps: 
-  
-# First step define a method that takes in a string as an argument
-def create_grocery_list(words_seperated_by_spaces)
-
+# input: string of words separated by spaces
+# steps:
   # Use a string method to to create an array determined by words separated by spaces.
-  grocery_keys = words_seperated_by_spaces.split
-
   # Iterate over the array to create keys and default values
-  grocery_list = Hash[ grocery_keys.map { |k| [k, nil] } ] 
-
   # print the list to the console using print method (we write)
-
-
-# output: [what data type goes here, array or hash?]
+  # output: [what data type goes here, array or hash?]
+def create_grocery_list(words_seperated_by_spaces)
+  grocery_keys = words_seperated_by_spaces.split
+  grocery_list = Hash[ grocery_keys.map { |k| [k, 0] } ]
+  print_list(grocery_list)
   return grocery_list
 end
 
-p create_grocery_list("eggs cereal milk pizza")
-
-
-
-# - - - - 
-
+# - - - -
 # Method to add an item to a list
 
-  # Create a method with 3 parameters: list, item name, and optional quantity
-
+# Create a method with 3 parameters: list, item name, and optional quantity
   # steps:
-    # Check to see if the item name is one of the keys in the hash
-      # If the key is in the hash
-        # print an error message: item is already in the hash
-      # If the key is not in the hash
-        # Add the item:
-          # hash[item name] = optional quantity
-
+  # Check to see if the item name is one of the keys in the hash
+    # If the key is in the hash
+      # print an error message: item is already in the hash
+    # If the key is not in the hash
+      # Add the item:
+        # hash[item name] = optional quantity
   # output:
     # Return the hash.
+def add_item(grocery_list, item_name, quantity=0)  
+ if grocery_list.has_key?(item_name.downcase)
+    puts "Item is already in the list"
+  else
+    grocery_list[item_name.downcase] = quantity
+  end
+  print_list(grocery_list)
+end
 
-
-# - - - - 
-
+# - - - -
 # Method to remove an item from the list
 # input: the hash, item (key)
 # steps:
@@ -50,13 +60,18 @@ p create_grocery_list("eggs cereal milk pizza")
     # remove the item
   # else if the item is not in the hash
     # print an error message.
-
 # output:
   # Return the hash
+def remove_item(grocery_list, item_name)
+  if grocery_list.has_key?(item_name.downcase)
+    grocery_list.delete(item_name)
+  else
+    puts "Item is not in grocery list"
+  end
+  print_list(grocery_list)
+end
 
-# - - - - 
-
-
+# - - - -
 # Method to update the quantity of an item
 # input: the hash, the desired item to update, updated quantity
 # steps:
@@ -64,17 +79,24 @@ p create_grocery_list("eggs cereal milk pizza")
     # Update the value at that key
   # else if the item is not in the hash
     # print an error message.
-
 # output:
   # Return the hash
+def update_quantity(grocery_list, item_name, quantity)
+  if grocery_list.has_key?(item_name.downcase)
+    grocery_list[item_name] = quantity
+  else
+    puts "Item is not in grocery list"
+  end
+  print_list(grocery_list)
+end
 
-# - - - - 
+# - - - -
+# test code
 
-
-# Method to print a list and make it look pretty
-# input: the hash
-# steps:
-  # iterate over the hash
-    # print the key and it's value
-# output:
-  # Printed output
+some_list = create_grocery_list("eggs carrots apples pizza")
+add_item(some_list, "lemonade", 2)
+add_item(some_list, "tomatoes", 3)
+add_item(some_list, "onions", 1)
+add_item(some_list, "Ice Cream", 4)
+remove_item(some_list, "lemonade")
+update_quantity(some_list, "ice cream", 1)

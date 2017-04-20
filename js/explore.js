3b912021fd6f3a5c30f9e0566f0408ba53411a0e
Alex Katzen <malex640@gmail.com>
2017-04-19 17:46:15 -0700

    Add explore.js with pseudocode

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+function reverse(word) {
+  //declare a variable 'reversedWord' that holds an empty string
+  //save length of the given word
+  //in a loop, iterate through the given word starting at the end
+    //add character to reversedWord.
+  //return reversedWord
+}

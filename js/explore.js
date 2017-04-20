6e75dd79a49a97d238a626d28a82cf60a1c3a58d
Alex Katzen <malex640@gmail.com>
2017-04-19 18:06:07 -0700

    Add function to reverse word

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,17 @@
 function reverse(word) {
   //declare a variable 'reversedWord' that holds an empty string
+  var reversedWord = "";
+
   //save length of the given word
+  var wordLength = word.length;
+  
   //in a loop, iterate through the given word starting at the end
-    //add character to reversedWord.
-  //return reversedWord
+  for (i = wordLength-1; i>=0; i--) { 
+    //add character to reversedWord
+    reversedWord += word.charAt(i)
+  }
+  
+  return reversedWord
 }
+
+console.log(reverse("Crystal"));

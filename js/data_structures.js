1e108fdc5cd1e0966c1d0f39e1f417271fb39547
Alex Katzen <malex640@gmail.com>
2017-04-20 16:52:33 -0700

    Add solution to Release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -18,7 +18,7 @@ addHorseName("Fluffy");
 console.log(colors);
 console.log(horseNames);
 
-
+//PSEUDOCODE
 // create a function that takes in both arrays of the same length (keys_array and values_array)
   // if keys_array and values_array are not the same length, return an error.
 
@@ -28,3 +28,29 @@ console.log(horseNames);
     // for each item in the keys array, assign that item as a key who's value is the corresponding item in the value array.
 
   // return the newObject
+
+
+
+// create a function that takes in both arrays of the same length (keys_array and values_array)
+
+function makeRanch(horses, colors) {
+  // if keys_array and values_array are not the same length, return an error.
+  if (horses.length != colors.length) {
+    console.log("Please provide two lists of the same length.");
+  } else {
+
+  // create a variable 'newObject' to hold the object that we create.
+  var newObject = {};
+
+  // loop through the keys_array from the beginning to the end.
+    for (var i = 0; i<= horses.length-1; i++) {
+
+    // for each item in the keys array, assign that item as a key who's value is the corresponding item in the value array.
+    newObject[horses[i]] = colors[i];
+
+    }
+    return newObject;
+  }
+}
+
+console.log(makeRanch(horseNames, colors));

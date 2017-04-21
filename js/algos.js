ef60771d5acc53b2ebbbfcd144681fccfea4c44a
Alex Katzen <malex640@gmail.com>
2017-04-20 19:16:47 -0700

    Add pseudocode for Release 0

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// RELEASE 0
+// write a function that takes an array of words/phrases and returns the longest one.
+
+// Pseudocode
+
+// Declare a function that takes in an array.
+// Create a variable 'longestWord' that holds an empty string.
+// Loop through the words in the array:
+  // If the [current item] is longer than 'longestWord',
+    // assign the current item to 'longestWord'.
+// return longestWord

d50d5d463d8eee8f6ab6d1ae9f44253af0de7657
Alex Katzen <malex640@gmail.com>
2017-04-28 11:38:30 -0700

    Add basic pseudocode structure for viewing/adding/editing albums

--- databases/awesome/music_collection.rb
+++ databases/awesome/music_collection.rb
@@ -18,17 +18,21 @@ create_albums_table = <<-SQL
   )
 SQL
 
-# execute SQLite query
 db.execute(create_albums_table)
 
 # add a test album
 db.execute( "INSERT INTO albums(album_name, artist, genre, year_released, date_added) VALUES ('Still Waters Run Deep', 'FourTops', 'Soul', 1970, 1231231)" )
 
 def add_album(db, album_name, artist, genre, year_released)
-  date_added = Time.new.to_s
+  date_added = Time.new.to_s[0..18]
 
   db.execute("INSERT INTO albums(album_name, artist, genre, year_released, date_added) VALUES (?, ?, ?, ?, ?)", [album_name, artist, genre, year_released, date_added])
 end
+
+
+# Create a method for viewing all the albums
+# Create a method for adding an album
+# Create a method for editing an album
 
 
 while true
@@ -66,3 +70,9 @@ while true
   add_album(db, album["album name"], album["artist"], album["genre"], album["year_released"])
 
 end
+
+# DRIVER CODE:
+# Create a interface that offers a menu of things to do:
+  # See the album collection
+  # Add an album to the collection
+  # Edit an album in the collection

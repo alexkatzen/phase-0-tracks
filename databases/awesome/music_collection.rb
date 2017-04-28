035c53079e99e2b012e1a91c0397c7ad77907d1c
Alex Katzen <malex640@gmail.com>
2017-04-27 22:22:55 -0700

    Add basic user interface to gather album info

--- databases/awesome/music_collection.rb
+++ databases/awesome/music_collection.rb
@@ -23,3 +23,37 @@ db.execute(create_albums_table)
 
 # add a test album
 db.execute( "INSERT INTO albums(album_name, artist, genre, year_released, date_added) VALUES ('Still Waters Run Deep', 'FourTops', 'Soul', 1970, 1231231)" )
+
+
+while true
+
+  album = { "album name" => nil, "artist" => nil, "genre"=> nil, "year released" => nil }
+
+  puts "Add an album to the collection! (or type 'exit' to quit)"
+  puts ""
+  puts "What is the name of the album?"
+  album["album name"] = gets.chomp
+
+  puts "Which artist is this album by?"
+  album["artist"] = gets.chomp
+
+  puts "What genre is this album?"
+  album["genre"] = gets.chomp
+
+  puts "What year was this album released"
+  album["year released"] = gets.chomp
+
+  puts ""
+  puts "Here's the album as entered so far:"
+  puts ""
+
+  album.keys.each_with_index do |info, index|
+    puts "#{index} - #{info.upcase}... #{album[info]}"
+  end
+  # About each_with_index: https://apidock.com/ruby/Enumerable/each_with_index
+
+  puts ""
+  puts "To add the album as-is, type 'add', to make edits type 'edit', otherwise type 'x' to start over"
+  answer = gets.chomp.downcase
+
+end

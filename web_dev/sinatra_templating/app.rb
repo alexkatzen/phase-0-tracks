4a884f78f45e57424564871296e6190d27b068f2
Alex Katzen <malex640@gmail.com>
2017-05-03 20:26:35 -0700

    Build a form and post route to add campuses

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,6 +7,29 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+# Create a for campuses
+create_campuses = <<-SQL
+  CREATE TABLE IF NOT EXISTS campuses(
+    id INTEGER PRIMARY KEY,
+    campus_name VARCHAR (255)
+  )
+SQL
+
+db.execute(create_campuses)
+
+# show campuses
+get '/campuses' do
+  @campuses = db.execute("SELECT * FROM campuses")
+  erb :campuses
+end
+
+# add a campus
+post '/campuses' do
+  db.execute("INSERT INTO campuses (campus_name) VALUES (?)", [params['campus_name']])
+  redirect '/campuses'
+  erb :campuses
+end
+
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
